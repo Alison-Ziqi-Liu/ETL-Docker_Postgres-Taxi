175b6b1e88a5de7ee34e209ca5f4a7b611de441b
Alison-Ziqi-Liu <86323722+Alison-Ziqi-Liu@users.noreply.github.com>
2024-11-29 23:09:37 -0500

    Add files via upload

---
+++ Bash Scripts.sh
@@ -0,0 +1,34 @@
+docker network create pg-network
+
+docker run -it \
+    -e POSTGRES_USER='root'\
+    -e POSTGRES_PASSWORD="root"\
+    -e POSTGRES_DB='ny_taxi'\
+    -p 5432:5432\
+    --network=pg-network \
+    --name pg-database\
+    postgres:13
+
+
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    --network=pg-network \
+    --name pgadmin\
+    dpage/pgadmin4
+
+
+docker build -t taxi_ingest:v001 .
+
+
+docker run -it \
+    --network=pg-network \
+    taxi_ingest:v001 \
+        --user=root \
+        --password=root \
+        --host=pg-database \
+        --port=5432 \
+        --db=ny_taxi \
+        --tb=yellow_taxi_trips \
+        --url="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9.1
+RUN apt-get install wget
+RUN pip install pandas==2.0.0
+RUN pip install sqlalchemy 
+RUN pip install psycopg2
+RUN pip install pyarrow
+RUN pip install numpy==1.26.4
+
+WORKDIR /app
+COPY ingest_data.py ingest_data.py
+
+ENTRYPOINT [ "python" ,"ingest_data.py"]
